bb088f3a884b4d5112cb774c245134e788092eb7
Larissa Pagliarin <pagll1@bfh.ch>
2023-12-17 21:55:47 +0100

    Chasing Stars

--- rezeptbuch/main.js
+++ rezeptbuch/main.js
@@ -17,3 +17,51 @@ if (homeSliderElement) {
     });
 }
 
+//Chasing Stars
+// Get the stars and background elements
+const stars = document.getElementById("stars");
+const backG = document.getElementById("backG");
+
+// Create a function to generate a single star
+function generateStar(randomAngle = Math.floor(Math.random(100) * 360)) {
+  const star = document.createElement("div");
+  star.classList.add("singleStar");
+
+  // Set the star's position and size
+  const left = Math.floor(Math.random() * 100);
+  const top = Math.floor(Math.random() * 100);
+  const size = Math.floor(Math.random() * 4);
+  star.style.width = size + "px";
+  star.style.height = size + "px";
+  star.style.left = (left / 100) * 100 + "%";
+  star.style.top = (top / 100) * 100 + "%";
+
+  // Add the star to the stars element
+  stars.appendChild(star);
+
+  // Add a mouseover event listener to the star
+  star.addEventListener("mouseover", () => {
+    // Rotate the star
+    star.style.rotate = `${
+      Math.floor(Math.random(100) * 360) - randomAngle / 5
+    }deg`;
+
+    // Add the shootingStar class to the star
+    star.classList.add("shootingStar");
+
+    // Remove the star after 2 seconds
+    setTimeout(() => {
+      star.style.display = "none";
+    }, 2000);
+  });
+}
+
+// Generate 300 stars
+for (let i = 0; i <= 300; i++) {
+  generateStar();
+}
+
+setTimeout(() => {
+  const h1 = document.getElementById("twoSec");
+  stars.removeChild(h1);
+}, 2000);
